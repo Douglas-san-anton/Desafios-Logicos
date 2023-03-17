a2c08406cfc1c6e36a92a9ee6df84e4ed105a7fe
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-03-17 18:48:09 -0300

    new challenge created

---
+++ Challenges/3-ComplementaryDNA.js
@@ -0,0 +1,14 @@
+// El ácido desoxirribonucleico(ADN) es una sustancia química que se encuentra en el núcleo de las
+// células y lleva las "instrucciones" para el desarrollo y funcionamiento de los organismos vivos.
+
+// Si quieres saber más: http://en.wikipedia.org/wiki/DNA
+
+// En las cadenas de ADN, los símbolos "A" y "T" son complementarios entre sí, como "C" y "G".
+// Su función recibe un lado del ADN(cadena, excepto Haskell); necesitas devolver el otro lado complementario.
+// La hebra de ADN nunca está vacía o no hay ADN en absoluto(de nuevo, a excepción de Haskell).
+
+// Más ejercicios similares se encuentran aquí: http://rosalind.info/problems/list-view/ (fuente)
+
+function DNAStrand(dna) {
+  //your code here
+}
