52d73c7047f16c45a8f9c4f515fcab8ee1275aa4
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-03-21 21:25:57 -0300

    new challenge created

---
+++ Challenges/5-ListFiltering.js
@@ -0,0 +1,10 @@
+// En este kata, creará una función que toma una lista de enteros y cadenas no negativos y devuelve una nueva lista con las cadenas filtradas.
+
+//Ejemplo:
+// filter_list([1, 2, 'a', 'b']) == [1, 2]
+// filter_list([1, 'a', 'b', 0, 15]) == [1, 0, 15]
+// filter_list([1, 2, 'aasf', '1', '123', 123]) == [1, 2, 123]
+
+function filter_list(l) {
+  // Devuelve una nueva matriz con las cadenas filtradas
+}
